1777dc2710606d84b1fd55984bf1be1d90360870
kyh <kkyyhh96@163.com>
2017-02-13 00:28:49 +0800

    2-13

--- baiduNews.py
+++ baiduNews.py
@@ -1,19 +1,55 @@
 # coding:utf-8
 # version:python3.5.1
 # author:kyh
+import queue
+import re
+
 import requests
-from bs4 import BeautifulSoup
 
-words=["东城区","西城区","朝阳区","丰台区","石景山区","海淀区","门头沟区","房山区","通州区","顺义区","昌平区","大兴区","怀柔区","平谷区","密云区","延庆区"]
-#百度新闻类,包括url,时间,题目,需要查询的字符串
+words = ["东城区", "西城区", "朝阳区", "丰台区", "石景山区", "海淀区", "门头沟区", "房山区", "通州区", "顺义区", "昌平区", "大兴区", "怀柔区", "平谷区", "密云区",
+         "延庆区"]
+
+
+# 百度新闻类,包括url,时间,题目,需要查询的字符串
 class baiduNews(object):
-    def __init__(self,main_word,url,date):
-        self.words=words
-        self.main_word=main_word
-        self.url=url
-        self.date=date
+    def __init__(self, main_word, url, date):
+        self.words = words
+        self.main_word = main_word
+        self.url = url
+        self.date = date
 
-    def write_info(self):
-        file=open("{0}.txt".format(self.main_word),'a')
-        file.writelines("{0},{1}\n".format(self.url,self.date))
-        file.close()
+    # 匹配字符串
+    def string_matching(self, file):
+        try:
+            request = requests.get(url=self.url, timeout=10)
+            # 如果编码是gb18030
+            self.main_word = "东城区"
+            request.encoding = 'gb18030'
+            word = re.findall(r'{0}'.format(self.main_word).encode('utf-8'), request.text.encode('utf-8'))
+            if word.__len__() == 0:
+                # 如果编码是utf-8
+                request.encoding = 'utf-8'
+                word = re.findall(r'{0}'.format(self.main_word).encode('utf-8'), request.text.encode('utf-8'))
+
+            # 如果长度大于0则解析成功,可以进行其他字符串的匹配
+            if word.__len__() > 0:
+                for matching_word in words:
+                    result = re.findall(r'{0}'.format(matching_word).encode('utf-8'), request.text.encode('utf-8'))
+                    if result.__len__() > 0:
+                        file.writelines("1,")
+                    else:
+                        file.writelines("0,")
+            else:
+                self.queue_word = queue.Empty
+
+        except Exception as e:
+            print(e)
+
+    # 输入每一条网页的所有信息
+    def write_info(self, file):
+        try:
+            file.writelines("\n{0},{1},".format(str(self.url), str(self.date)))
+
+            self.string_matching(file)
+        except Exception as e:
+            print(e)
--- get_baidu_news.py
+++ get_baidu_news.py
@@ -3,16 +3,16 @@
 # author:kyh
 
 import requests
+import time
 from bs4 import BeautifulSoup
-
 from baiduNews import baiduNews
 
 
 # 传入关键词,获取一页所有的新闻内容(20条)
-def search_words(url, words):
+def search_words(url, words, count):
     params = {
         "word": str(words),
-        "pn": "0",
+        "pn": str(count),
         "cl": "2",
         "ct": "1",
         "tn": "news",
@@ -25,22 +25,26 @@ def search_words(url, words):
     return request.text
 
 
-# 提取出新闻的日期和url,如果结束返回True
+# 提取出本页的新闻,如果结束返回True,如果所有新闻查询完毕,返回False
 def translate_url(html, words):
     soup = BeautifulSoup(html, 'html.parser')
-    count=1
-    while count<=20:
-        if create_news(soup,words,count):
-            count+=1
+    count = 0
+    while count < 20:
+        if create_news(soup, words, count):
+            count += 1
         else:
-            return True
+            return False
     return True
 
 
-def create_news(soup, words,count):
+# 提取新闻日期和url,如果提取成功返回True,否则返回False
+def create_news(soup, words, count):
     try:
         # 新闻的日期
         news_date = soup.find_all("div", class_="result")[count].find('p', class_="c-author").string
+    except Exception as e:
+        return False
+    try:
         year = str(news_date).split('年')[0][-4:]
         month = str(news_date).split('年')[1].split('月')[0]
         day = str(news_date).split('月')[1].split('日')[0]
@@ -48,12 +52,23 @@ def create_news(soup, words,count):
         # 新闻的url
         news_url = soup.find_all('div', {'class': 'result'})[count].find('a')['href']
         baidu_news = baiduNews(words, news_url, news_date)
+        baidu_news.write_info(file)
         return True
     except Exception as e:
-        return False
+        return True
 
 
 url = "http://news.baidu.com/ns"
+#传入关键词
 words = "西城区 东城区 朝阳区 供暖"
-html = search_words(url, words)
-translate_url(html, words)
+#打开文件
+file=open("{0}.txt".format(words),'a')
+count = 0
+html = search_words(url, words, count)
+while translate_url(html, words):
+    count += 20
+    print(count)
+    #防止百度封ip,设置为1s
+    time.sleep(1)
+    html = search_words(url, words, count)
+file.close()
