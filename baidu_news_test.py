d2549d5a5f77f7992e71bc42aac36a5eedc2a576
kyh <kkyyhh96@163.com>
2017-02-17 23:40:18 +0800

    2-17

--- baidu_news_test.py
+++ baidu_news_test.py
@@ -4,6 +4,7 @@
 
 import requests
 import datetime
+import time
 from bs4 import BeautifulSoup
 import re
 
@@ -16,6 +17,10 @@ def compute_date(y0,m0,d0,y1,m1,d1):
         temp_time+=datetime.timedelta(days=1)
 
 
+words0 ='(东城区 | 西城区 | 朝阳区 | 丰台区 | 石景山区 | 海淀区)'
+words1='(门头沟区 | 房山区 | 通州区 | 顺义区 | 昌平区 | 大兴区)'
+words2='(怀柔区 | 平谷区 | 密云区 | 延庆区)'
+compute_date(2017,1,6,2017,2,10)
 
 url_test="http://www.beijing.gov.cn/zfzx/qxrd/hdq/t1257589.htm"
 headers={
