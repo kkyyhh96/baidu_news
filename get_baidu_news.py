eb85b6df025520cce3176df223a250c9ab2a52f1
kyh <kkyyhh96@163.com>
2017-02-18 23:21:16 +0800

    2-18

--- get_baidu_news.py
+++ get_baidu_news.py
@@ -34,13 +34,16 @@ def search_words(url, words, count, begin_time='0', end_time='0'):
 
 
 # 提取出本页的新闻,如果结束返回True,如果所有新闻查询完毕,返回False
-def translate_url(html, words):
+def translate_url(html, words, html_set):
     try:
         soup = BeautifulSoup(html, 'html.parser')
         count = 0
         while count < 20:
-            if create_news(soup, words, count):
+            set_length=html_set.__len__()
+            if create_news(soup, words, count,html_set):
                 count += 1
+                if set_length== html_set.__len__():
+                    return False
             else:
                 return False
         return True
@@ -50,7 +53,7 @@ def translate_url(html, words):
 
 
 # 提取新闻日期和url,如果提取成功返回True,否则返回False
-def create_news(soup, words, count):
+def create_news(soup, words, count,html_set):
     try:
         # 新闻的日期
         news_date = soup.find_all("div", class_="result")[count].find('p', class_="c-author").string
@@ -63,6 +66,7 @@ def create_news(soup, words, count):
         news_date = "{0}-{1}-{2}".format(year, month, day)
         # 新闻的url
         news_url = soup.find_all('div', {'class': 'result'})[count].find('a')['href']
+        html_set.add(news_url)
         baidu_news = baiduNews(words, news_url, news_date)
         baidu_news.write_info(file)
         return True
@@ -78,6 +82,7 @@ end_date = datetime.date(2017, 1, 31)
 temp_time = start_date
 while temp_time <= end_date:
     print(temp_time)
+    # 计算日期
     begin_time = temp_time
     temp_time += datetime.timedelta(days=1)
     end_time = temp_time
@@ -89,10 +94,11 @@ while temp_time <= end_date:
     file = open("1.txt".format(words), 'a')
     count = 0
     html = search_words(url, words, count, begin_time, end_time)
+    html_set = set()
     # 如果百度新闻出现问题,重新发送请求
     while html is None:
         html = search_words(url, words, count, begin_time, end_time)
-    while translate_url(html, words):
+    while translate_url(html, words, html_set):
         count += 20
         print(count)
         # 防止百度封ip,设置为1s
